8fbe6cc957367baa9f2aee523d41efdfb52fcdc7
23DesignStudio <23design.dev@gmail.com>
2019-08-14 11:25:01 +0800

    add new demo

--- Platform_io/nodemcu32s_ws2812/src/main.cpp
+++ Platform_io/nodemcu32s_ws2812/src/main.cpp
@@ -1,9 +1,22 @@
 #include <Arduino.h>
+#include <FastLED.h>
 
-void setup() {
-  // put your setup code here, to run once:
+#define NUM_LEDS 16
+#define DATA_PIN 2
+
+CRGB leds[NUM_LEDS];
+
+void setup()
+{
+  FastLED.addLeds<NEOPIXEL, DATA_PIN>(leds, NUM_LEDS);
+
+  for (byte i = 0; i < NUM_LEDS; i++)
+  {
+    leds[i] = CRGB::Red;
+  }
+  FastLED.show();
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop()
+{
 }
